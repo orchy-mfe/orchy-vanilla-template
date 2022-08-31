076ce6d6059210b91ab457105dd30a7d8fbae6c7
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-08-31 23:04:58 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import './style.css'
+import {renderWithQiankun,qiankunWindow} from "vite-plugin-qiankun/dist/helper";
+import viteLogo from './assets/vite.svg?raw'
+import javascriptLogo from './assets/javascript.svg?raw'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      ${viteLogo}
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      ${javascriptLogo}
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+
+renderWithQiankun({
+  mount(props) {
+    render(props);
+  },
+  bootstrap() {},
+  unmount() {
+    root.unmount();
+    root._container.innerHTML = "";
+    root = null;
+  },
+});
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({});
+}
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import {defineConfig} from 'vite'
+import {visualizer} from 'rollup-plugin-visualizer'
+import qiankun from 'vite-plugin-qiankun'
+
+const port = process.env.PORT || 3000
+
+export default defineConfig(({mode}) => ({
+  plugins: [
+    qiankun('orchy-vanilla-template', {useDevMode: true}),
+    visualizer(),
+  ],
+  base: mode === 'development' ? `http://localhost:${port}/` : '/orchy-vanilla-template/',
+  server: {port},
+  test: {
+    environment: 'happy-dom',
+    mockReset: true
+  }
+}))
